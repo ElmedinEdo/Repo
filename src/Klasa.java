b45b81e1a4ed62b73b3ef421086ff2271da45ef4
ElmedinEdo <edohanic@hotmail.com>
2018-10-18 18:30:56 +0200

    poruka

--- src/Klasa.java
+++ src/Klasa.java
@@ -4,7 +4,7 @@ public class Klasa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
     
-		System.out.println("Promjne koda");
+		System.out.println("Promjene koda");
 	}
 
 }
